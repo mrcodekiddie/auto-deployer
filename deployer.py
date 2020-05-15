08d8d0ea949a98ee8f705771455b62ef6fc86a6a
mrcodekiddie <vmuthu@tuta.io>
2020-05-15 06:26:12 +0530

    killed ports finally 🔪

--- deployer.py
+++ deployer.py
@@ -4,18 +4,43 @@ import datetime
 import yaml
 import shutil
 import socket
-
+import subprocess
+import re
 
 #GITHUB_AUTH_TOKEN=(os.environ["GITHUB_AUTH_TOKEN"])
 USER_HOME="/home/timelord" #os.environ['HOME']
 LOG_PATH=USER_HOME+"/logs/"
 NVM_BIN="/home/timelord/.nvm/versions/node/v12.16.3/bin"#os.environ['NVM_BIN']
-g=Github("Sollaa matten")
+g=Github("bowwww")
 
 
 with open('config.yml') as f:
     config =yaml.load(f,Loader=yaml.FullLoader)
     print(type(config))
+
+def isOpen(ip,port):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      s.connect((ip, int(port)))
+      s.shutdown(2)
+      return True
+   except:
+      return False
+
+# def get_pids(port):
+
+# 	command = "sudo lsof -i :%s | awk '{print $2}'" % port
+# 	pids = subprocess.check_output(command, shell=True)
+# 	pids = pids.strip()
+# 	if pids:
+# 		pids = re.sub(' +', ' ', str(pids))
+# 		for pid in pids.split('\n'):
+# 			try:
+# 				yield int(pid)
+# 			except:
+# 				pass
+#
+#SAVED FOR LATER USE
 
 
 for repo in g.get_user().get_repos():
@@ -56,21 +81,29 @@ for repo in g.get_user().get_repos():
                             f.close
                             os.system("systemctl enable "+repo.name+".service")
                             os.system("systemctl start "+repo.name+".service")
+
                     else:
                         if(isOpen('127.0.0.1',configData['port'])):
                             print("port is open")
+                            port = str(configData['port'])
+                            process_kill_command = "sudo kill $(sudo lsof -t -i:"+port+")"
+                            print(process_kill_command)
+                            print("port "+port+" is killed, Command status:"+os.system(process_kill_command))
+                            os.system("systemctl stop "+repo.name+".service")
+                            os.system("systemctl start "+repo.name+".service")
+                            os.system('restarting service')
+                        else:
+                            print("someone fucked the port already\n restarting service")
+                            os.system("systemctl stop "+repo.name+".service")
+                            os.system("systemctl start "+repo.name+".service")
+
+
+
 
                 print(sshCloneUrl)
                 print(commit.sha)
 
-def isOpen(ip,port):
-   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   try:
-      s.connect((ip, int(port)))
-      s.shutdown(2)
-      return True
-   except:
-      return False
+
       
             
             
